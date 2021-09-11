e445531b868b564f2f63fcb6d953ae6cd0ff4eb4
Andrés Falco <FalcoGamer2016@hotmail.com>
2021-09-11 00:20:15 -0500

    Cambios en el Startup

--- MITIENDA.BlazorServer/Startup.cs
+++ MITIENDA.BlazorServer/Startup.cs
@@ -15,6 +15,7 @@ namespace MITIENDA.BlazorServer
 {
     public class Startup
     {
+        //EJEMPLOS DE MODIFICACIONES
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -22,8 +23,6 @@ namespace MITIENDA.BlazorServer
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
